4a75534c66238e3679f24cfa054aa62683da9333
Harvoid <ky0915111@naver.com>
2025-03-23 17:10:36 +0900

    Design:마이페이지 탈퇴 기능 모달 창 구현

--- src/component/mypage/mypage.js
+++ src/component/mypage/mypage.js
@@ -4,15 +4,18 @@ import MypageApi from "../../api/MypageApi";
 import { useParams } from "react-router-dom";
 import ChatApi from '../../api/ChatApi';
 import style from "../../Css/MyPage.module.css";
+import Modal from "react-modal";
+
 
 //회원 정보 수정
-function MyPage({ userData }) { 
+function MyPage({ userData }) {
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const token = localStorage.getItem("accessToken")//사용자 토큰
 
   //초기 상태 설정 (userData 있으면 사용, 없으면 기본값)
   const [formData, setFormData] = useState(userData || {
     id: "",
-    nick:"",
+    nick: "",
     pwd: "",
     pwdCheck: "",
     phone: "",
@@ -26,32 +29,32 @@ function MyPage({ userData }) {
   //사용자 정보 불러오기
   useEffect(() => {
     const fetchUserInfo = async () => {
-        try {
-            const res = await ChatApi.getUserInfo(token);//api 요청
-            // console.log(res.data);
-            // 기존 formData의 기본값을 유지하면서 데이터 업데이트
-            setFormData(prevState => ({
-                ...prevState, 
-                ...res.data //기존값 유지하면서 새로운 값 추가
-            }));
-        } catch (error) {
-            console.error("로그인 정보 가져오기 실패:", error);
-        }
+      try {
+        const res = await ChatApi.getUserInfo(token);//api 요청
+        // console.log(res.data);
+        // 기존 formData의 기본값을 유지하면서 데이터 업데이트
+        setFormData(prevState => ({
+          ...prevState,
+          ...res.data //기존값 유지하면서 새로운 값 추가
+        }));
+      } catch (error) {
+        console.error("로그인 정보 가져오기 실패:", error);
+      }
     };
 
     fetchUserInfo();
-}, [token]); 
+  }, [token]);
 
 
-    // axios.get("http://localhost:8080/mymy/userinfo/me", { })
-    //   .then(response => {
-    //     console.log("로그인된 사용자:", response.data);
-    //     setFormData(response.data); //로그인된 사용자 정보로 상태 업데이트
-    //   })
-    //   .catch(error => {
-    //     console.error("로그인 정보 가져오기 실패:", error);
-    //   });
-  
+  // axios.get("http://localhost:8080/mymy/userinfo/me", { })
+  //   .then(response => {
+  //     console.log("로그인된 사용자:", response.data);
+  //     setFormData(response.data); //로그인된 사용자 정보로 상태 업데이트
+  //   })
+  //   .catch(error => {
+  //     console.error("로그인 정보 가져오기 실패:", error);
+  //   });
+
 
   //입력값 변경 핸들러
   const handleChange = (e) => {
@@ -78,7 +81,7 @@ function MyPage({ userData }) {
     setError("");
 
     // 필수 입력 필드 검사
-    if (!formData.nick ||!formData.pwd || !formData.pwdCheck || !formData.email || !formData.phone) {
+    if (!formData.nick || !formData.pwd || !formData.pwdCheck || !formData.email || !formData.phone) {
       setError("모든 필드를 입력해야 합니다.");
       return;
     }
@@ -99,11 +102,11 @@ function MyPage({ userData }) {
       alert(`${field === "pwd" ? "비밀번호" : field === "email" ? "이메일" : field === "nick" ? "닉네임" : "전화번호"}를 입력해주세요.`);
       return;
     }
-  
+
     try {
       const updateData = { id: formData.id, [field]: formData[field] }; // 수정할 데이터 구성성
       const res = await MypageApi.modify(updateData);//api 요청청
-  
+
       if (res.status === 200) {
         alert(`${field === "pwd" ? "비밀번호" : field === "email" ? "이메일" : field === "nick" ? "닉네임" : "전화번호"} 성공적으로 변경되었습니다!`);
       }
@@ -112,6 +115,9 @@ function MyPage({ userData }) {
     }
   };
 
+  const openDeleteModalBtn = () => {
+    setOpenDeleteModal(!openDeleteModal);
+  };
   //회원 탈퇴 처리
   const handleDeleteAccount = async () => {
     if (!token) {
@@ -136,67 +142,81 @@ function MyPage({ userData }) {
       <h1>회원 정보 수정</h1>
       <hr className={style.hr} />
       <form onSubmit={handleSubmit}
-      className={style.formContainer}>
-        <div className={style.form}>
-          <label>아이디</label>
-          <input type='text'className={`${style.readOnlyId}`}  value={formData.id} readOnly />
-          {/* 간격을 맞추기 위한 버튼 (화면상에서 보이지 않음) */}
+        className={style.grid} id='userInfoModifyForm'>
+
+
+        <div className={style.gridItem}>
+          <label className={style.label}>아이디</label>
+          <input type='text' className={`${style.readOnlyId}`} value={formData.id} readOnly />
+
           <button type="button" readOnly className={style.readonly}>변경</button>
-
-
         </div>
-        <div className={style.form}>
-          <label>닉네임</label>
+
+        <div className={style.gridItem}>
+          <label className={style.label}>닉네임</label>
           <input className={`Shadow`} type="text" name="nick" value={formData.nick} onChange={handleChange} />
-          <button type="button" onClick={() => handleUpdateField("nick")}>변경</button>
+          <button type="button" onClick={() => handleUpdateField("nick")} className={style.modifybutton}>변경</button>
         </div>
 
-        <div className={style.form}>
-          <label>비밀번호</label>
-          <input className={`Shadow`}  type="password" name="pwd" value={formData.pwd} onChange={handleChange} />
+        <div className={style.gridItem}>
+          <label className={style.label}>비밀번호</label>
+          <input className={`Shadow`} type="password" name="pwd" value={formData.pwd} onChange={handleChange} />
           <button type="button" readOnly className={style.readonly}>변경</button>
 
 
         </div>
 
-        <div className={style.form}>
-          <label>비밀번호 확인</label>
+        <div className={style.gridItem}>
+          <label className={style.label}>비밀번호 확인</label>
           <input className={`Shadow`} type="password" name="pwdCheck" value={formData.pwdCheck} onChange={handleChange} />
-          <button type="button" onClick={() => handleUpdateField("pwd")}>변경</button>
+          <button type="button" onClick={() => handleUpdateField("pwd")} className={style.modifybutton}>변경</button>
         </div>
         {/* 비밀번호 오류 메시지 표시 */}
         {error && <p style={{ color: "red" }}>{error}</p>}
 
-        <div className={style.form}>
-          <label>이메일</label>
-          <input className={`Shadow`}  type="email" name="email" value={formData.email} onChange={handleChange} />
-          <button type="button" onClick={() => handleUpdateField("email")}>변경</button>
+        <div className={style.gridItem}>
+          <label className={style.label}>이메일</label>
+          <input className={`Shadow`} type="email" name="email" value={formData.email} onChange={handleChange} />
+          <button type="button" onClick={() => handleUpdateField("email")} className={style.modifybutton} >변경</button>
         </div>
 
-        <div className={style.form}>
-          <label>전화번호</label>
+        <div className={style.gridItem}>
+          <label className={style.label}>전화번호</label>
           <input className={`Shadow`} type="text" name="phone" value={formData.phone} onChange={handleChange} />
-          <button type="button" onClick={() => handleUpdateField("phone")}>변경</button>
+          <button type="button" onClick={() => handleUpdateField("phone")} className={style.modifybutton}>변경</button>
         </div>
-
-        <button className={style.submitBtn} type="submit">저장</button>
-        <button type="button" onClick={handleDeleteAccount} className={style.deleteAccountBtn}>
-            회원 탈퇴</button>
-
       </form>
+
+      <button className={style.submitBtn} type="submit" form='userInfoModifyForm'>저장</button>
+
+
       {/* 회원 탈퇴 처리 */}
-      <hr />
+
+
       <div>
-        <h2>회원 탈퇴</h2>
-        <div>
-          <input 
-            type="checkbox" 
-            checked={keepPosts} 
-            onChange={() => setKeepPosts(!keepPosts)} 
+
+        <button type="button" onClick={openDeleteModalBtn} className={style.deleteAccountBtn}>
+          회원 탈퇴</button>
+
+        <Modal
+          isOpen={openDeleteModal}
+          ariaHideApp={true}
+          onRequestClose={openDeleteModalBtn}
+          className={`Shadow modal`}
+        >
+          <label className={style.label}>게시글을 남기고 탈퇴하시겠습니까?</label>
+          <span>게시글 삭제를 원하신다면 체크를 풀어주세요!</span>
+          <input
+            type="checkbox"
+            checked={keepPosts}
+            onChange={() => setKeepPosts(!keepPosts)}
           />
-          <label>게시글 남기고 탈퇴</label>
-        </div>
-        {deleteError && <p style={{ color: 'red' }}>{deleteError}</p>}
+
+          {deleteError && <p style={{ color: 'red' }}>{deleteError}</p>}
+
+          <button type="button" onClick={handleDeleteAccount} className={style.deleteAccountBtn}>
+            탈퇴</button>
+        </Modal>
       </div>
 
 
