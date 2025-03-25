94cd06a1027950b9e295d9dd2011d44ae51d895c
Harvoid <ky0915111@naver.com>
2025-03-25 12:52:26 +0900

    Revert "Feat:여행자 게시판 작성자 아이디에 회원 페이지 링크 추가"
    This reverts commit 606b8cd05f0c483866952b5e7a2b4932a186faa0.

--- src/Components/Board/MateBoardList.js
+++ src/Components/Board/MateBoardList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import MateBoardApi from "../../api/MateBoardApi";
 import style from "../../Css/BoardList.module.css";
 
@@ -111,7 +111,7 @@ const MateBoardList = () => {
                   }}>
                   <td>{index + 1}</td>
                   <td>{post.title}</td>
-                  <td><Link to={`../../profile/${post.id}`} className={`link`}>{post.id}</Link></td>
+                  <td>{post.id}</td>
                   <td>{!!post.date ? post.date.substring(0, 13) : "날짜 없음"}</td>
                   <td>{post.repCnt}</td>
                   <td>{post.boardCnt}</td>
